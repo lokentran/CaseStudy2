a7dcf99d84f70a301dc1b4a0921ec3a973496b28
lokentran <thanhtungmarine@gmail.com>
2020-07-26 06:24:47 +0700

    Connect database

---
+++ src/Model/DBConnect.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+class DBConnect
+{
+    protected $dsn;
+    protected $username;
+    protected $password;
+
+    public function __construct()
+    {
+        $this->dsn = "mysql:host=localhost;dbname=demoCase;charset=utf8";
+        $this->username = 'root';
+        $this->password = '1234Tung@123';
+    }
+
+    public function connect()
+    {
+        try {
+            
+            return new \PDO($this->dsn, $this->username, $this->password);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
